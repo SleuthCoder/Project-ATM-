63facd303ee156386b68ccfe584dee2f5182ece2
Sahasrasmi Yalamanchili <sahasrasmi.yalamanchili@gmail.com>
2021-03-01 14:00:52 +0530

    Create ATM.py

---
+++ ATM.py
@@ -0,0 +1,12 @@
+class ATM(object)
+def __init__(self, cardNumber, pinNumber)
+
+self.pinNumber = pinNumber
+self.cardNumber = cardNumber
+
+def CashWithdrawal(self)
+print ("Withdraw Your Cash")
+
+
+def BalanceEnquiry(self)
+print ("Here Is Your Balance")
